2f8e2e75d6c31f83a20ac57a90236a32621da23e
Laser Nite <lasernite@gmail.com>
2013-10-07 23:24:12 -0400

    m

--- mitweb/settings.py
+++ mitweb/settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mitweb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'posgres',
+        'PASSWORD': 'password',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -108,6 +108,7 @@ ROOT_URLCONF = 'mitweb.urls'
 WSGI_APPLICATION = 'mitweb.wsgi.application'
 
 TEMPLATE_DIRS = (
+	'/Users/lasernite/mitweb/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
